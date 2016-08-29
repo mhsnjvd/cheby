2a55b4d47ac0bf63da3d2af6bcf7ea358e03200e
Mohsin Javed <mhsnjvd@gmail.com>
2016-08-29 08:55:02 +0100

    progress

--- test_chebtech.py
+++ test_chebtech.py
@@ -1,23 +1,29 @@
 import numpy as np
 from chebtech.chebtech import Chebtech
-from chebtech.cumsum import cumsum
-from chebtech.roots import roots
+#from chebtech.roots import roots
 
+if __name__ == "__main__":
+    f = Chebtech(coeffs=[0, 0, 1])
+    f.roots()
+    print(f.coeffs)
+    g = Chebtech(coeffs=[0, 1])
+    print('roots of x are:')
+    print(g.roots())
 
-f = Chebtech(coeffs=[0, 0, 1])
-g = Chebtech(coeffs=[0, 1])
-l = Chebtech(coeffs=[0, 0, 0, 1])
-print(roots(l))
-h = f + g
-k = g + f
-print( h.coeffs)
-print( k.coeffs)
-print( 'f[0] = %s' % f[0] )
+    l = Chebtech(coeffs=[0, 0, 0, 1])
+    #print(roots(l))
+    h = f + g
+    k = g + f
+    print( h.coeffs)
+    print( k.coeffs)
+    print( 'f[0] = %s' % f[0] )
 
-#f.plot()
-g = cumsum(f)
-#g.plot()
+    #f.plot()
+    g = f.cumsum()
+    #g.plot()
 
+    f = Chebtech(coeffs=[1, .75, 0, .25])
+    print(f.coeffs)
 
-r = roots(f)
-print(r)
+    #r = roots(f)
+    #print(r)
